c963b23ea5b34f715218e8faf9564aaacba88f10
caiaandrei14 <caiaandrei14@hotmail.com>
2026-01-02 21:24:39 +0200

    connect server to rabbit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer connection.Close()
+	fmt.Println("Peril server connected to rabbit")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Peril server is shutting down")
+	os.Exit(0)
 }
