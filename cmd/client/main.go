764e1f0bf6dccff48ad5bafc177192a8dc26ce43
caiaandrei14 <caiaandrei14@hotmail.com>
2026-01-02 22:58:47 +0200

    add client, declare queue and REPL for server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	conStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	user, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+user,
+		routing.PauseKey,
+		pubsub.SimpleQueueTransient)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Peril client is shutting down")
+	os.Exit(0)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -28,16 +27,45 @@ func main() {
 		return
 	}
 
-	err = pubsub.PublishJson(ch, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	gamelogic.PrintServerHelp()
 
-	if err != nil {
-		fmt.Println(err)
-		return
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			fmt.Println("Sending pause message")
+			err = pubsub.PublishJson(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				})
+
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "resume":
+			fmt.Println("Sending resume message")
+			err = pubsub.PublishJson(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				})
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "quite":
+			fmt.Println("Peril server is shutting down")
+			return
+		default:
+			fmt.Println("Unknown command")
+		}
 	}
-
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Peril server is shutting down")
-	os.Exit(0)
 }
