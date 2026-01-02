22eea6392747cfa8d85af58a70fda5954aaf811c
caiaandrei14 <caiaandrei14@hotmail.com>
2026-01-02 22:05:13 +0200

    publish pause message from the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,20 +5,35 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Starting Peril server...")
 
-	connectionString := "amqp://guest:guest@localhost:5672/"
-	connection, err := amqp.Dial(connectionString)
+	conStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conStr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer connection.Close()
+	defer conn.Close()
 	fmt.Println("Peril server connected to rabbit")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = pubsub.PublishJson(ch, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
